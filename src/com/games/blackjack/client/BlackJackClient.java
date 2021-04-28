341990d89da88e5b33db466d4fb4e94836e72066
Tom Fulton <tomfulton.mentor@gmail.com>
2021-04-27 22:40:18 -0400

    Client Deck

--- src/com/games/blackjack/client/BlackJackClient.java
+++ src/com/games/blackjack/client/BlackJackClient.java
@@ -1,7 +1,11 @@
 package com.games.blackjack.client;
 
+import com.games.blackjack.Dealer;
+import com.games.blackjack.Deck;
+
 public class BlackJackClient {
     public static void main(String[] args) {
         System.out.println("Blackjack Game Initialized");
+        Dealer dealer = new Dealer("Dealer", new Deck());
     }
 }
